82c24d0eff112f23c08ed6d8007e5d294a728163
user-at-host <sergeygrishuk1995@gmail.com>
2025-07-25 17:41:33 +0300

    Add more tests

--- tests.py
+++ tests.py
@@ -10,5 +10,13 @@ class TestDarkScan(TestCase):
         self.assertTrue(check_ports("1-11"))
         self.assertTrue(check_ports("1,2,3"))
 
+        self.assertFalse(check_ports("99999"))
+        self.assertFalse(check_ports("1,1.1"))
+        self.assertFalse(check_ports("1-9-19"))
+
+    def test_ipv4_addresses(self):
+        self.assertTrue(check_ipv4_address("1.1.1.1"))
+
+
 if __name__ == '__main__':
     main()
